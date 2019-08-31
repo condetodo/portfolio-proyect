6dfaa9f4fc8c5fa8daccd95a04ce83975c9a1bf9
francisco <franezeperez@gmail.com>
2019-08-31 12:57:08 -0300

    boostrap in jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()    
+    image = models.ImageField(upload_to='images/')
+    
+
+
+# Create a blog model.
+
+# add blog app to settings.
+# create a migration.
+# migrate.
+# add to the admin.
